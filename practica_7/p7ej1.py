d03bbc90b57b6a427a1bfb4048a8b8be0a0e6bc1
Tobi Maggiori <169007210+tobimaggiori@users.noreply.github.com>
2024-06-05 20:05:16 -0300

    t

--- practica_7/p7ej1.py
+++ practica_7/p7ej1.py
@@ -1,7 +1,7 @@
 def crear_carta(valor, palo):
     # Listas de valores y palos válidos
-    valores_validos = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-    palos_validos = ['corazones', 'diamantes', 'tréboles', 'picas']
+    valores_validos = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
+    palos_validos = ('corazones', 'diamantes', 'tréboles', 'picas')
     
     # Validación del valor y el palo
     if valor not in valores_validos:

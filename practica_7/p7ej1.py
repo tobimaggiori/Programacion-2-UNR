c004acb1dfcf1e298c7acdcb5f35671867b51421
tobimaggiori <tobiasmaggiori@outlook.com>
2024-06-06 00:39:16 -0300

    rf

--- practica_7/p7ej1.py
+++ practica_7/p7ej1.py
@@ -1,15 +1,21 @@
-def crear_carta(valor, palo):
-    # Listas de valores y palos válidos
-    valores_validos = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
-    palos_validos = ('corazones', 'diamantes', 'tréboles', 'picas')
-    
-    # Validación del valor y el palo
-    if valor not in valores_validos:
-        raise ValueError(f"Valor inválido: {valor}. Los valores válidos son: {valores_validos}")
-    if palo not in palos_validos:
-        raise ValueError(f"Palo inválido: {palo}. Los palos válidos son: {palos_validos}")
-    
-    # Retorna la carta como una tupla
-    return (valor, palo)
+# Representacion de una carta de bajara francesa:
+# Una carta de baraja francesa es: (valor, palo)
+# Donde valor puede ser cualquier elemento de la siguiente tupla:
+# (2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A')
+# Y palo puede ser cualquier elemento de la siguiente tupla:
+# ('corazones', 'diamantes', 'tréboles', 'picas')
+# De no cumplise estas condiciones, el elemento no es una carta de baraja francesa.
 
-    # probando si esto se sube a git
+def crear_carta(valor, palo):
+    valores_validos = (2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A')
+    palos_validos = ('corazones', 'diamantes', 'tréboles', 'picas')
+
+    if valor in valores_validos:
+        if palo in palos_validos:
+            retorno = (valor, palo)
+        else:
+             retorno = f"Palo inválido: {palo}. Los palos válidos son: {palos_validos}"
+    else:
+        retorno = f"Valor inválido: {valor}. Los valores válidos son: {valores_validos}"
+    return retorno
+
